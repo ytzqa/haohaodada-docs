43235c9838fc146d99999c8c0a93a96c2d68cf44
Blue <529155318@qq.com>
2018-10-17 10:54:03 +0800

    修改logo

--- conf.py
+++ conf.py
@@ -102,7 +102,7 @@ html_theme_options = {
 }
 
 
-html_logo = 'logo/logo.jpg'
+html_logo = 'logo/logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
